467d59d2687b4eea804c48e3a79e1cfeb823d72f
logansmith6 <logsmith06@gmail.com>
2021-07-13 13:36:51 -0700

    added basic form in addUser and a handleOnChange function

--- src/components/Users/addUser.js
+++ src/components/Users/addUser.js
@@ -7,4 +7,28 @@ class addUser extends Component {
         email: '',
         password: ''
     }
+
+    handleOnChange = event => {
+        if (event.target.name === 'content'){
+          this.setState({
+            ...this.state,
+            content: event.target.value
+          })
+        } else {
+          this.setState({
+            ...this.state,
+            author: event.target.value
+          })
+        }
+      }
+
+    render() {
+        return (
+          <div className="container">
+            <form>
+                <textarea />
+            </form>
+          </div>
+        );
+      }
 }
