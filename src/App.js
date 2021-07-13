1ad3469310a43991f0dce8f3e9d18c4c28ae1867
logansmith6 <logsmith06@gmail.com>
2021-07-13 14:30:04 -0700

    logout now working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Login from './components/Users/Login';
-
+import Logout from './components/Users/Logout'
 
 
 import './App.css';
@@ -12,6 +12,7 @@ class App extends Component {
     <div className="App">
       <h1>Sign Up</h1>
        <Login />
+       <Logout />
     </div>
   );
  }
