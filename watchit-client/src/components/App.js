1b35645e9acf2766095d664dece37652261805ad
logansmith6 <logsmith06@gmail.com>
2021-07-23 11:24:47 -0700

    added userscontainer

--- watchit-client/src/components/App.js
+++ watchit-client/src/components/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
-
+import UsersContainer from './UsersContainer'
 
 
 const App = () => {
     return (
-        <div className="App">
-        
-      
+        <div>
+            <UsersContainer />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <form class="example" action="action_page.php">
           <input type="text" placeholder="Search.." name="search"/>
           <button type="submit"><i class="fa fa-search"></i></button>
         </form>
         <a href='/'><img className="main-logo"src="https://i.imgur.com/WYhzzOk.png"/></a>
-        
-       
-      
-      
     </div>
     )
 }
---
+++ watchit-client/src/components/UsersContainer.js
@@ -0,0 +1,13 @@
+//container for users... methods and data
+
+import React, { Component } from 'react';
+
+class UsersContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
