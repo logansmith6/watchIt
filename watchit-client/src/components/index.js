0c01b2c3f9d7412dbdd1e00b23984cb813367486
logansmith6 <logsmith06@gmail.com>
2021-07-23 10:40:50 -0700

    added react-redux and redux-thunk

---
+++ watchit-client/src/components/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import './index.css';
+
+//will have a redux store
+
+
+ReactDOM.render(
+  
+      <App />,
+    
+    
+  
+  document.getElementById('root')
+);
+
