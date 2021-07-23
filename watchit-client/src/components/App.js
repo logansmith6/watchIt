f54cb9d64e26ef7fbc42705cb32af2c7e09e7089
logansmith6 <logsmith06@gmail.com>
2021-07-23 11:38:34 -0700

    added a basic fetch users action, just to get a foundation down. it will need tweaking and will primarily be used for vidoes, not users.

--- watchit-client/src/components/App.js
+++ watchit-client/src/components/App.js
@@ -5,7 +5,7 @@ import UsersContainer from './UsersContainer'
 const App = () => {
     return (
         <div>
-            <UsersContainer />
+            <UsersContainer /> 
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <form class="example" action="action_page.php">
           <input type="text" placeholder="Search.." name="search"/>
--- watchit-client/src/components/UsersContainer.js
+++ watchit-client/src/components/UsersContainer.js
@@ -1,13 +1,18 @@
 //container for users... methods and data
 
 import React, { Component } from 'react';
-
+//connect will allow use of actions
+import { connect } from 'redux'
+import { fetchUsers } from '../actions/usersActions'
 class UsersContainer extends Component {
     render() {
         return (
             <div>
-                
+
             </div>
         )
     }
 }
+
+export default connect(null, { fetchUsers }(UsersContainer));
+//this makes fetchUsers available to the container as a prop
