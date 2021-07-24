cdd6997e01c4303cbffa13b67d84261f553e8186
logansmith6 <logsmith06@gmail.com>
2021-07-24 15:21:08 -0700

    going to be using react and redux for user login

---
+++ watchit-client/src/components/Login.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+}
