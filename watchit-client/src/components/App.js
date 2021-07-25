600aa52625b84e4f27dd981d186bb70b0f75f1aa
logansmith6 <logsmith06@gmail.com>
2021-07-24 19:14:13 -0700

    added cool spinning logo for when the page is loading

--- watchit-client/src/components/App.js
+++ watchit-client/src/components/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import '../App.css';
 
 import Router from './Router';
+import { useAuth0 } from '@auth0/auth0-react'
 //import PostsContainer from './PostsContainer';
 
 import AuthenticationButton from '../components/Registration/AuthenticationButton';
+import Loading from "../components/Registration/Loading";
 
 import Account from '../components/Registration/Account';
 
@@ -12,7 +14,15 @@ import Account from '../components/Registration/Account';
 
 
 const App = () => {
+  
+  const { isLoading } = useAuth0();
+  if (isLoading) {
+    return <Loading />
+  }
+
     return (
+
+      
       
       <div>
         <Router />
